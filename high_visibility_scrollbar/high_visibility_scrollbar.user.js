13226edbc7353881a0049aee449cf5779f934292
lgoldstien <lgoldstien@gmail.com>
2013-07-02 14:27:04 +0200

    Add correct functions in userscript

--- high_visibility_scrollbar/high_visibility_scrollbar.user.js
+++ high_visibility_scrollbar/high_visibility_scrollbar.user.js
@@ -8,9 +8,44 @@
 // @grant				GM_addStyle
 // ==/UserScript==
 
-// Make the style sheet load from Github repo
-var link = window.document.createElement('link');
-link.rel = 'stylesheet';
-link.type = 'text/css';
-link.href = 'https://raw.github.com/lgoldstien/userscripts/stable/high_visibility_scrollbar/style.css';
-document.getElementsByTagName("head")[0].appendChild(link);
+// The script object
+
+var hvs = {
+	// Add a stylesheet
+	addStyle: function(stylesheeturl) {
+		var stylesheet = window.document.createElement('link');
+		link.rel = 'stylesheet';
+		link.type = 'text/css';
+		link.href = stylesheeturl;
+		document.getElementsByTagName("head")[0].appendChild(stylesheet);
+	},
+	// Add a script
+	addScript: function(scripturl) {
+		var script = window.document.createElement('script');
+		link.src = scripturl;
+		document.getElementsByTagName("body")[0].appendChild(script);
+	},
+	// Wrap the contents of the body
+	wrapContent: function(classname) {
+		$('body').wrapInner('<div class="' + classname + '" />');
+	}
+}
+
+// Run when we have everything set and loaded
+window.onload = function() {
+	// Add jquery if not already there
+	if (typeof $ == 'undefined') {  
+	    hvs.addScript("https://github.com/lgoldstien/userscripts/blob/stable/high_visibility_scrollbar/jquery.js");
+	}
+	// Add the custom scrollbar script
+	hvs.addScript("https://github.com/lgoldstien/userscripts/blob/stable/high_visibility_scrollbar/jquery.custom-scrollbar.js");
+	// Add the css files
+	hvs.addStyle("https://github.com/lgoldstien/userscripts/blob/stable/high_visibility_scrollbar/jquery.custom-scrollbar.css");
+	hvs.addStyle("https://github.com/lgoldstien/userscripts/blob/stable/high_visibility_scrollbar/style.css");
+
+	// Wrap the body
+	hvs.wrapContent("_hvs_body");
+
+	// Run the required bits
+	$("._hvs_body").customScrollbar();
+});
