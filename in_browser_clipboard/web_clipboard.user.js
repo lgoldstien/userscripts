2543407b64ff1de384810c2c89f2918e06152af1
lgoldstien <lgoldstien@gmail.com>
2013-07-09 17:29:25 +0200

    Add localStorage to allow storage of clipboard items

--- in_browser_clipboard/web_clipboard.user.js
+++ in_browser_clipboard/web_clipboard.user.js
@@ -1,36 +1,51 @@
 // ==UserScript==
-// @name 				The Web Clipboard
-// @description 		This script will give you a clipboard on every webpage you visit
-// @version 			0.1
-// @namespace 			http://lgoldstien.github.io/userscripts
-// @downloadURL 		https://raw.github.com/lgoldstien/userscripts/stable/in_browser_clipboard/web_clipboard.user.js
-// @updateURL			https://raw.github.com/lgoldstien/userscripts/stable/in_browser_clipboard/web_clipboard.user.js
-// @grant				GM_setClipboard
+// @name                The Web Clipboard
+// @description         This script will give you a clipboard on every webpage you visit
+// @version             0.1
+// @namespace           http://lgoldstien.github.io/userscripts
+// @downloadURL         https://raw.github.com/lgoldstien/userscripts/stable/in_browser_clipboard/web_clipboard.user.js
+// @updateURL           https://raw.github.com/lgoldstien/userscripts/stable/in_browser_clipboard/web_clipboard.user.js
+// @require             http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js
+// @grant               GM_setClipboard
 // ==/UserScript==
 
-// The clipboard functions
+$(document).ready(function(){
+    $(window).keydown(function (e) {
+        clipboard.getClipboard(e);
+    });
+});
 
+
+// The clipboard functions
 var clipboard = {
 
-	var clipboard = [];
+    setClipboard: function (text) {
+        
+        GM_setClipboard(text);
 
-	setClipboard: function (text) {
-		
-		GM_setClipboard(text);
+    },
 
-	},
+    getClipboard: function (e) {
+        if (e.ctrlKey && e.keyCode === 67) {
+            var content = window.getSelection();
+            this.storeItem(content);
+        }
+    },
 
-	getClipboard: function () {
-		if (e.ctrlKey && e.keyCode === 67) {
-	        alert("Text copied was: " + window.getSelection());
-	    }
-	},
+    storeItem: function (content) {
+        var row = this.storageCurrentRows();
+        row = row + 1;
+        localStorage.setItem("web_clipboard_" + row, content);
+        localStorage.setItem("web_clipboard_rows", row);
+        console.log("Copied an item to storage row " + row + " with following content: " + content)
+    },
+
+    storageCountRows: function () {
+        return localStorage.getItem("web_clipboard_rows");
+    },
+
+    storageCurrentRows: function () {
+        return this.storageCountRows() + 1;
+    }
 
 }
-
-
-$(window).keyup(function (e) {
-    if (e.ctrlKey && e.keyCode === 67) {
-        alert("Text copied was: " + window.getSelection());
-    }
-});
